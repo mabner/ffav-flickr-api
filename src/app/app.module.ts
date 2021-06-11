48d207f824f2a8f5145a6128602b0e9381d8fa4c
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-11 02:48:32 -0300

    fixes NullInjectorError: No provider for HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PhotoGalleryComponent } from './components/photo-gallery/photo-gallery.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule( {
   declarations: [
@@ -12,7 +12,8 @@ import { PhotoGalleryComponent } from './components/photo-gallery/photo-gallery.
     PhotoGalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
