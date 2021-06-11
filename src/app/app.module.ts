3c3eccb5a6dcbf55c6828b6281f3b221eda4bdd1
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-11 01:40:07 -0300

    photo gallery base files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PhotoGalleryComponent } from './components/photo-gallery/photo-gallery.component';
 
 @NgModule( {
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PhotoGalleryComponent
   ],
   imports: [
     BrowserModule
