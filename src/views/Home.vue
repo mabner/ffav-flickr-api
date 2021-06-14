cc514aaeaa1193973a0a564d50592e3714b690bd
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-14 02:19:57 -0300

    search implemented, returning json response

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,20 +9,55 @@
         Buscar
       </button>
     </form>
+    <p v-if="loading">Carregando...</p>
+    <ul v-else>
+      <li v-for="image in images" :key="image.id">{{ image }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import config from '../../config';
+
 export default {
   name: 'home',
   data() {
     return {
       tag: '',
+      loading: false,
+      images: [],
     };
   },
   methods: {
     search() {
+      // Turns loading message on
+      this.loading = true;
+
+      this.fetchImages().then((response) => {
+        this.images = response.data.photos.photo;
+
+        // Turns loading message off
+        this.loading = false;
+      });
       console.log('Buscando por: ', this.tag);
+    },
+    fetchImages() {
+      return axios({
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          // Full list of parameters: https://www.flickr.com/services/api/flickr.photos.search.html
+          method: 'flickr.photos.search',
+          api_key: config.api_key,
+          tags: this.tag,
+          extras: 'url_n, owner_name, date_taken, views',
+          page: 1,
+          format: 'json',
+          nojsoncallback: 1,
+          per_page: 30,
+        },
+      });
     },
   },
 };
